d52c20197781b92349a8ccb9141cc3d3d24162b4
MarcAnthony-WebDev <marcanthony.webdev@gmail.com>
2024-08-21 20:00:13 -0400

    navbar and footer completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { footerLinks } from "@/constants";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mt-5 flex flex-col border-t border-gray-100 text-black-100">
+      <div className="flex flex-wrap justify-between gap-5 px-6 py-10 max-md:flex-col sm:px-16">
+        <div className="flex flex-col items-start justify-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            width={118}
+            height={36}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            Carhub 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  href={item.url}
+                  className="text-gray-500"
+                  key={item.title}
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-10 flex flex-wrap items-center justify-between border-t border-gray-100 px-6 py-10 sm:px-16">
+        <p>@2023 CarHub. All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link className="text-gray-500" href="/">
+            Privacy Policy
+          </Link>
+          <Link className="text-gray-500" href="/">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
